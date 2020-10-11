25033f3b86a1ae6dc577df6719965859553dc1b6
Blake Ramsdell <blake.ramsdell@onelogin.com>
2020-10-11 12:51:54 -0700

    Add empty console project

---
+++ Bcr.Example.Gitflow/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bcr.Example.Gitflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
